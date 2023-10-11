7c5cb96a8597624b3a096f03f4b94304bc0105d3
pranav vadnere <93157765+pranavvadner@users.noreply.github.com>
2023-10-11 09:49:53 +0000

    convertion of data type

---
+++ javascript/datatypes.js
@@ -0,0 +1,18 @@
+"use strict";// treat all js cde as newer version 
+
+//alert(3+3)// we are using nodejs not browser
+//undefined is type and null is object
+
+//let score=null
+//console.log(typeof(score));
+//let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+
+//"33" => 33
+// "33abc"=>Nan
+//true=>1 false=>0
+let isLoggedIn=1
+let booleanissloggedIn=Boolean(isLoggedIn)
+console.log(booleanissLoggedIn);
